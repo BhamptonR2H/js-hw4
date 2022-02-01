6245d643c5c894e48f6953998724c97e1562368e
Brenden Hampton <brendenhampton@R2Hs-MacBook-Pro.local>
2022-01-31 22:52:36 -0500

    completed hard

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function median(arr) {
 let nums = [4,5,6,7,0,1,2]
 let firstNums = (numArr, target) => numArr.indexOf(target)
 console.log(firstNums(nums,2))
+
+// Hard
+// I created to functions one that turns the background red and one that turns it white
+const chgColor = () =>{
+  document.body.style.background = 'red';
+}
+const changeColor = () =>{
+  document.body.style.background = 'white';
+}
+// Not sure if you wanted me to do it the way the challenge says but this worked as well
