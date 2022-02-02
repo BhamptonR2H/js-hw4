1b42a8dd54087d3cb93d0326db23e5ce11287890
Brenden Hampton <brendenhampton@R2Hs-MacBook-Pro.local>
2022-02-01 21:35:46 -0500

    finished very hard

--- script.js
+++ script.js
@@ -23,3 +23,25 @@ const changeColor = () =>{
   document.body.style.background = 'white';
 }
 // Not sure if you wanted me to do it the way the challenge says but this worked as well
+// Very Hard
+// Created function called coinChange with two parameters
+var coinChange = (coins, amount) => {
+  const table = new Array(amount + 1).fill(Infinity);
+  table[0] = 0;
+
+  // iterate over the coins
+  for(let coin of coins) {
+      for(let i = 0; i < table.length; i++) {
+          if(coin <= i) {
+              let idx = i - coin;
+              let potentialAmt = table[idx] + 1;
+              // updating the current iteration in table
+              table[i] = Math.min(potentialAmt, table[i]);
+          }
+      }
+  }
+  return table[table.length - 1] === Infinity ? -1 : table[table.length - 1];
+};
+
+console.log(coinChange([1,2,5], 11));
+console.log(coinChange([2],3))
